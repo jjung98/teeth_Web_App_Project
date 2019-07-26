7be640b1fce9b5c936956e2a187a3fd5fbb1ffa7
jjung98 <ung546834@gmail.com>
2019-07-26 17:54:05 +0900

    Spring and vue interlocking configuration settings We have to develop now! :(

---
+++ src/main/java/com/example/demo/TeethApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeethApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeethApplication.class, args);
+	}
+
+}
