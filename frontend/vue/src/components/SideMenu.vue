f73f656badd7e4fc59cf0666b202eda808875ffb
jjung98 <ung546834@gmail.com>
2019-08-02 16:43:19 +0900

    submenu editting-01

--- frontend/vue/src/components/SideMenu.vue
+++ frontend/vue/src/components/SideMenu.vue
@@ -1,43 +1,37 @@
 <!-- 여기는 서브메뉴를 만들어 놓은 컴포넌트야! -->
 <template>
-  <div id="submenu">
+  <div>
     <div>
-      <img ref="refLogo" v-bind:src='img_logo'>
-    </div>
-    <div class="menu">
-      <ul ref="reful">
-        <li>
-          <a>&ndash; 치매백과사전</a>
-        </li>
-        <li>
-          <a>&ndash; 치매자가진단</a>
-        </li>
-        <li>
-          <a>&ndash; 오랄 필라테스</a>
-        </li>
-        <li>
-          <a>&ndash; 치매 예방 게임</a>
-          <ul>
+      <div style="float: left;">
+        <img ref="refLogo" v-bind:src='img_logo'>
+        <div style="display: inline-block;">
+          <ul class="menu">
             <li>
-              <a>&rsaquo; 파타카라 운동</a>
+              <a v-bind:href="link_dire">치매백과사전</a>
             </li>
             <li>
-              <a>&rsaquo; 같은 그림 찾기</a>
+              <a v-bind:href="link_test">치매자가진단</a>
+            </li>
+            <li>
+              <a v-bind:href="link_filla">오랄 필라테스</a>
+            </li>
+            <li>
+              <a>치매 예방 게임</a>
             </li>
           </ul>
-        </li>
-        <li>
-          <a>&ndash; 내 정보</a>
-        </li>
-      </ul>
+          <ul class="submenu"  ref="refsumenu">
+            <li>
+              <a>같은 그림찾기</a>
+            </li>
+            <li>
+              <a>파타카라 운동</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <router-view ref="refUserInfo" id="user_info">
+      </router-view>
     </div>
-    <div ref="refcreater" class="creater">
-      <p>만든사람들</p>
-      <p>강준희 곽승민 김세훈</p>
-      <p>송지연 장유정 허승연</p>
-    </div>
-    <router-view ref="refUserInfo" id="user_info">
-    </router-view>
   </div>
 </template>
 
@@ -47,77 +41,47 @@ export default {
   data () {
     return {
       img_logo: require('../assets/teeth_logo.png'),
-      link_login: 'http://localhost:8080/login',
-      link_sign: 'http://localhost:8080/sign'
+      link_dire: 'http://localhost:8080/dire',
+      link_test: 'http://localhost:8080/test',
+      link_filla: 'http://localhost:8080/filla',
+      link_game: 'http://localhost:8080/game'
     }
-  },
-  created () {
-    const imgLogWidth = this.$refs.refLogo.width
-    const imgLogHeight = this.$refs.refLogo.height
-
-    const ulObject = this.$refs.reful
-    const createrObject = this.$refs.refcreater
-    const userinfoObject = this.$refs.refUserInfo
-
-    ulObject.style.width = (imgLogWidth - 60) + 'px'
-    ulObject.style.marginTop = imgLogHeight + 'px'
-    createrObject.style.width = (imgLogWidth - imgLogWidth / 5) + 'px'
-    createrObject.style.marginLeft = (imgLogWidth / 5) / 2 + 'px'
-    userinfoObject.style.height = imgLogHeight + 'px'
-  },
-  mounted () {
-    const imgLogWidth = this.$refs.refLogo.width
-    const imgLogHeight = this.$refs.refLogo.height
-
-    const ulObject = this.$refs.reful
-    const createrObject = this.$refs.refcreater
-
-    ulObject.style.width = (imgLogWidth - 60) + 'px'
-    ulObject.style.marginTop = imgLogHeight + 'px'
-    createrObject.style.width = (imgLogWidth - imgLogWidth / 5) + 'px'
-    createrObject.style.marginLeft = (imgLogWidth / 5) / 2 + 'px'
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-img{
-    width: fit-content;
-    height: fit-content;
-    float: left;
-    top: 0px;
-    bottom: 0px;
-    left: 0px;
-    right: 0px;
-    position: absolute;
+div img{
+  width: fit-content;
+  height: fit-content;
 }
 
 .menu{
-  position: fixed;
-  top: 0px;
-  bottom: 0px;
-  left: 0px;
-  right: 0px;
-  width: fit-content;
-  height: 100%;
-}
-
-ul{
+  display: inline-block;
   list-style: none;
   text-align: left;
   color: #000000;
-  padding: 50px 29px 30px 30px;
-  border-right: #C4DEFF solid 1px;
+  padding: 0px 30px 13px 90px;
   height: 100%;
+  font-family: Noto Sans Medium,AppleSDGothicNeo-Regular,Malgun Gothic,맑은 고딕,dotum,돋움,sans-serif;
 }
 
 ul li{
+  float: left;
   line-height: 200%;
   font-size: 14px;
 }
-ul li:hover{
-  color: #4374D9;
+ul li a{
+    padding: 0 7px 0 17px;
+    color: #6798FD;
+    font-size: 16px;
+    font-weight: 700;
+    text-decoration: none;
+}
+ul li a:hover{
+  color: #000000;
+  text-decoration: underline;
 }
 
 ul li ul{
@@ -126,29 +90,16 @@ ul li ul{
   margin-left: 25px;
 }
 
-.creater{
-  position: fixed;
-  bottom: 0px;
-  left: 0px;
-  right: 0px;
-  width: fit-content;
-  height: fit-content;
-  padding: 30px 0;
-  border: 1px dashed #C4DEFF;
-  margin-bottom: 20px;
-}
-.creater p{
-  margin: 0;
-  line-height: 200%;
-  font-size: 12px;
+.submenu{
+    display: block;
+    margin-left: 0;
+    padding-left: 40px;
+    list-style: none;
 }
 
 #user_info{
-  position: fixed;
-  top: 0px;
-  bottom: 0px;
-  right: 0px;
   margin-right: 3%;
-  margin-top: 4%;
+  float: right;
+  margin-top: 5%;
 }
 </style>
